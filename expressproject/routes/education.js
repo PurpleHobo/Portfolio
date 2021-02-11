bbc561395ef5cba09ef8531c5da0c5d435f4a619
PurpleHobo <cttrybula@gmail.com>
2021-02-11 01:24:03 +0000

    Added some css and other stuff

--- expressproject/routes/education.js
+++ expressproject/routes/education.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('education', { title: 'Education' });
 });
 
 module.exports = router;
